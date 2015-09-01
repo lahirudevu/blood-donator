492b243102760eb277e2751191ee545461db0d7d
Madhumal <madhumal@thinkcube.com>
2015-09-01 11:42:53 +0530

    added server config and route config files

--- skelton/server.js
+++ skelton/server.js
@@ -6,4 +6,7 @@ var router = require('./config/routes');
 app.use('/', router);
 app.listen(port);
 
+app.locals.router = express.Router();
+app.locals.express = express;
+
 console.log('Magic happens on port ' + port);
