47e6b54fa53d5ef7c448bc39d5a5d8ae436fcd33
Madhumal <madhumal@thinkcube.com>
2015-09-14 12:06:06 +0530

    added the policy adding capability

--- server/api/controllers/index.js
+++ server/api/controllers/index.js
@@ -1,9 +1,24 @@
 import express from 'express';
 
 var router = express.Router();
+var sessionAuth = require('../policies/sessionAuth');
 
+/**
+**This section binds the policies to relevant routes
+**/
+logger.info('loading the system policies');
+router.all("/user/*", sessionAuth, function(req, res, next) {
+  next();
+});
+
+
+
+
+/**
+**
+**This section defines routes
+**/
 logger.info('loading routes');
-
 router.use('/user', require('./UserController'));
 router.use('/request', require('./RequestController'));
 
