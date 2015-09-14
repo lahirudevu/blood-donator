f1863ae11be63f569f6747d230e2f8eb91243f19
Madhumal <madhumal@thinkcube.com>
2015-09-14 17:48:58 +0530

    add basic restfull api to requests

--- server/api/controllers/RequestController.js
+++ server/api/controllers/RequestController.js
@@ -11,7 +11,7 @@ router.post('/', (req, res) => {
 	.then((result)=>{
 
 		logger.info('created a new request ');
-		logger.debug(request);
+		logger.debug(result);
 
 		res.status(201).send(result);
 	})
@@ -19,47 +19,163 @@ router.post('/', (req, res) => {
 		logger.error(error);
 		res.status(400).send(error);
 	});
-	
-});
 
-//update an existing model or create new instance if not
-router.put('/', (req, res) => {
-	res.send('update an existing model or create new instance if not');
 });
 
 //update attributes for a model.
 router.put('/:id', (req, res) => {
-	res.send('update attributes for a model.');
+
+	let id = req.params.id;
+	let updateObj = req.body;
+	logger.info('updating model ' + id);
+
+	//update the object
+	models.request.update({"id":id}, updateObj)
+	.then((result)=>{
+
+		logger.info('updated the request ');
+		logger.debug(result);
+
+		res.status(200).send(result);
+	})
+	.catch((error)=>{
+		logger.error(error);
+		res.status(400).send(error);
+	});
+
 });
 
 //update instances of the model matching the criteria
 router.post('/update', (req, res) => {
-	res.send('update instances of the model matching the criteria.');
-});
 
-//find all instances matched by the filter
-router.get('/', (req, res) => {
-	res.send('find all instances matched by the filter');
-});
+	let filter = req.body.filter;
+	let updateObj = req.body.update;
+	logger.info('updating matcing models ');
 
-//get first instance matching the filter
-router.get('/findOne', (req, res) => {
-	res.send('get first instance matching the filter');
-});
+	//update the matching objects
+	models.request.update(filter, updateObj)
+	.then((result)=>{
 
-//get an instance by id
-router.get('/:id', (req, res) => {
-	res.send('get an instance by id');
-});
+		logger.info('updated request objs ');
+		logger.debug(result);
 
-//check weather an instance exists
-router.get('/:id/exists', (req, res) => {
-	res.send('check weather an instance exists');
+		res.status(200).send(result);
+	})
+	.catch((error)=>{
+		logger.error(error);
+		res.status(400).send(error);
+	});
 });
 
 //delete an instance by id
 router.delete('/:id', (req, res) => {
-	res.send('delete an instance by id');
+
+	let id = req.params.id;
+
+	models.request.destroy({"id":id})
+	.then((result)=>{
+
+		logger.info('deleted the request ');
+		logger.debug(result);
+
+		res.status(200).send(result);
+	})
+	.catch((error)=>{
+		logger.error(error);
+		res.status(400).send(error);
+	});
+});
+
+//get an instance by id
+router.get('/:id', (req, res) => {
+
+	let id = req.params.id;
+
+	models.request.findOne({"id":id})
+	.then((result)=>{
+
+		logger.info('finding a request ');
+		logger.debug(result);
+
+		res.status(201).send(result);
+	})
+	.catch((error)=>{
+		logger.error(error);
+		res.status(400).send(error);
+	});
+});
+
+//check weather an instance exists
+router.get('/:id/exists', (req, res) => {
+
+	let id = req.params.id;
+
+	models.request.findOne({"id":id})
+	.then((result)=>{
+
+		if(result && result.id){
+			logger.info('request obj exists');
+			res.status(200).send(true);
+		}else{
+			logger.info('request obj doesnt exist');
+			res.status(200).send(false);
+		}
+
+	})
+	.catch((error)=>{
+		logger.error(error);
+		res.status(400).send(error);
+	});
+});
+
+//find all instances matched by the filter
+router.get('/', (req, res) => {
+
+	let filter = req.query;
+
+	logger.debug('finding all requests filter : ');
+	logger.debug(filter);
+
+	models.request.find(filter)
+	.then((result)=>{
+
+		logger.info('finding all the request objs matching criteria');
+		res.status(200).send(result);
+
+	})
+	.catch((error)=>{
+		logger.error(error);
+		res.status(400).send(error);
+	});
+});
+
+//update an existing model or create new instance if not
+router.put('/', (req, res) => {
+
+	let filter = req.body.filter;
+	let updateObj = req.body.update;
+
+	//update the matching objects
+	models.request.find(filter)
+	.then((result)=>{
+
+		if(result.length>0){
+			logger.info('updating obj in update or create');
+			res.status(200);
+			return models.request.update(filter, updateObj);
+		}else{
+			logger.info('create obj in update or create');
+			res.status(201);
+			return models.request.create(updateObj);
+		}
+		
+	}).then((result)=>{
+		res.send(result);
+	})
+	.catch((error)=>{
+		logger.error(error);
+		res.status(400).send(error);
+	});
 });
 
 module.exports = router;
