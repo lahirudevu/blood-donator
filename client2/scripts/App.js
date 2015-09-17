68d835600db5a9c8d5b15cbe61424cac6865f0d3
Madhumal <madhumal@thinkcube.com>
2015-09-17 17:06:33 +0530

    remove document title package

--- client2/scripts/App.js
+++ client2/scripts/App.js
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react';
 import Explore from './components/Explore';
-import DocumentTitle from 'react-document-title';
 
 export default class App {
   static propTypes = {
@@ -9,13 +8,11 @@ export default class App {
 
   render() {
     return (
-      <DocumentTitle title='Sample App'>
         <div className='App'>
           <Explore {...this.props} />
           <hr />
           {this.props.children}
         </div>
-      </DocumentTitle>
     );
   }
 }
