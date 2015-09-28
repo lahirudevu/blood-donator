92b0de20de9d25aa044ebc63674001328de8bb69
Madhumal <madhumal@thinkcube.com>
2015-09-28 14:18:41 +0530

    added secondary buttons

--- bloodclient/src/css/btns.js
+++ bloodclient/src/css/btns.js
@@ -1,5 +1,75 @@
 var styles = {
 
+	btnPrimaryLarge: {
+
+	  fontFamily: "'Roboto', sans-serif",
+	  color: "#ffffff",
+	  fontSize: "14px",
+	  background: "#1F90FF",
+	  padding: "12px 30px 12px 30px",
+	  textDecoration: "none",
+	  transition : '0.8s',
+
+	  ':hover' : {
+	  	background: '#3cb0fd',
+	  	textDecoration: 'none'
+	  }
+
+	},
+
+	btnPrimary:{
+
+	  fontFamily: "'Roboto', sans-serif",
+	  color: "#ffffff",
+	  fontSize: "10px",
+	  background: "#1F90FF",
+	  padding: "10px 20px 10px 20px",
+	  textDecoration: "none",
+	  transition : '0.8s',
+
+	  ':hover' : {
+	  	background: '#3cb0fd',
+	  	textDecoration: 'none'
+	  }
+
+	},
+
+	btnSecondaryLarge: {
+
+	  fontFamily: "'Roboto', sans-serif",
+	  color: "#ffffff",
+	  fontSize: "14px",
+	  background: "transparent",
+	  padding: "12px 30px 12px 30px",
+	  textDecoration: "none",
+	  border: 'solid #1f628d 1px',
+	  transition : '0.8s',
+
+	  ':hover' : {
+	  	background: '#3cb0fd',
+	  	textDecoration: 'none'
+	  }
+
+	},
+
+	btnSecondary:{
+
+	  fontFamily: "'Roboto', sans-serif",
+	  color: "#ffffff",
+	  fontSize: "10px",
+	  background: "transparent",
+	  padding: "10px 20px 10px 20px",
+	  textDecoration: "none",
+	  border: 'solid #1f628d 1px',
+	  transition : '0.8s',
+
+	  ':hover' : {
+	  	background: '#3cb0fd',
+	  	textDecoration: 'none'
+	  }
+
+	},
+
 	btnoptLarge : {
 
 	  fontFamily: "'Roboto', sans-serif",
@@ -8,6 +78,7 @@ var styles = {
 	  background: '#FFA726',
 	  padding: '12px 30px 12px 30px',
 	  textDecoration: 'none',
+	  transition : '0.8s',
 
 	  ':hover': {
 		  background: '#f09009',
@@ -23,6 +94,7 @@ var styles = {
 	  background: '#FFA726',
 	  padding: '10px 20px 10px 20px',
 	  textDecoration: 'none',
+	  transition : '0.8s',
 
 	  ':hover': {
 		  background: '#f09009',
--- bloodclient/src/js/pages/Home.js
+++ bloodclient/src/js/pages/Home.js
@@ -15,7 +15,13 @@ class Home extends React.Component {
         return <div>
             <p>This is the home page</p>
             <a ref="mainBtn" style={styles.btnoptLarge} href="#">Save a Life</a><br/><br/>
-            <a ref="sideBtn" style={styles.btnopt} href="#">Save a Life</a><br/><br/>
+            <a ref="sideBtn" style={styles.btnopt} href="#">Save a Life</a><br/><br/><br/>
+
+            <a ref="secondaryLarge" style={styles.btnSecondaryLarge} href="#">Save a Life</a><br/><br/><br/>
+            <a ref="seBtn" style={styles.btnSecondary} href="#">Save a Life</a><br/><br/><br/>
+
+            <a ref="prLargeBtn" style={styles.btnPrimaryLarge} href="#">Primary large</a><br/><br/>
+            <a ref="prBtn" style={styles.btnPrimary} href="#">Primary</a><br/><br/>
             <Link to="/about" >About page</Link><br/>
             <Link to="/contributions" >contribution page</Link>
         </div>
