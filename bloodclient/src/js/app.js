f25519d1c7416787cafd716ff396a815d4bbb031
Iswan Jumat <iswan@thinkcube.com>
2015-09-29 14:03:19 +0530

    Changed react routing stucture for add common layout

--- bloodclient/src/js/app.js
+++ bloodclient/src/js/app.js
@@ -1,18 +1,28 @@
-import Home from './pages/Home';
-import About from './pages/About';
-import Contributions from './pages/Contributions';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Contributions from './pages/Contributions';
+// import createBrowserHistory from 'history/lib/createBrowserHistory';
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import {Router, Route} from 'react-router';
+//
+// let history = createBrowserHistory();
+//
+// //define routing and handlers
+// ReactDom.render((
+//   <Router history={history}>
+//     <Route name="home" path="/" component={Home} />
+//     <Route name="about" path="/about" component={About} />
+//     <Route name="contributions" path="/contributions" component={Contributions} />
+//   </Router>
+// ), document.getElementById('app'));
+
 import React from 'react';
 import ReactDom from 'react-dom';
-import {Router, Route} from 'react-router';
+import Router from 'react-router';
+import routes from './config/routes';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 let history = createBrowserHistory();
 
-//define routing and handlers
-ReactDom.render((
-  <Router history={history}>
-    <Route name="home" path="/" component={Home} />
-    <Route name="about" path="/about" component={About} />
-    <Route name="contributions" path="/contributions" component={Contributions} />
-  </Router>
-), document.getElementById('app'));
+ReactDom.render(<Router history={history}>{routes}</Router>, document.getElementById('app'));
