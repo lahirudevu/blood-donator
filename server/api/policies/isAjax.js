aca3f5e93eb371def2dff525e37130b2bbdd1ab0
Madhumal <madhumal@thinkcube.com>
2015-09-29 19:42:23 +0530

    form url encoded and multipart data request sent to the rest apientries

--- server/api/policies/isAjax.js
+++ server/api/policies/isAjax.js
@@ -2,11 +2,35 @@ module.exports = function (req, res, next) {
 
   let requestType = req.get('Content-Type'); 
 
-  if (requestType && requestType == 'application/json') {
-  	logger.info('application/json request');
-  	next();
-  }else{
-  	logger.info('not xhr method');
+  if(!requestType){
+  	logger.debug('reqtype not set');
   	res.render('index');
+  }else{
+
+  	  requestType = requestType.toLowerCase(); 
+
+	  let isjsonreq = false;
+	  let ismultipart = false;
+	  let isformurl = false;
+
+	  if(requestType == 'application/json')
+	  	isjsonreq = true;
+
+	  if(requestType.indexOf('multipart')>-1)
+	  	ismultipart = true;
+
+	  if(requestType.indexOf('form-urlencoded')>-1)
+	  	isformurl = true;
+
+	  //if multipart or json request
+	  if (isjsonreq || ismultipart || isformurl) {
+
+	  	logger.info('application/json or multipart request or form data');
+	  	next();
+	  }else{
+
+	  	logger.info('not xhr method');
+	  	res.render('index');
+	  }
   }
 }
