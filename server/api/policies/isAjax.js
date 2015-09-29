8971068666123c8c1421a8a30161a378d7e122fb
Madhumal <madhumal@thinkcube.com>
2015-09-29 20:48:00 +0530

    by pass auth urls in ajax check policy

--- server/api/policies/isAjax.js
+++ server/api/policies/isAjax.js
@@ -1,36 +1,63 @@
+//check if the url is neither ajax nor react router call
+function isByPassUrl(url){
+
+	if(url.indexOf('/auth')==0 || url.indexOf('/favicon.ico')==0){
+		return Promise.resolve(true);
+	}else{
+		return Promise.resolve(false);
+	}
+}
+
 module.exports = function (req, res, next) {
 
-  let requestType = req.get('Content-Type'); 
+	let orgUrl = req.originalUrl;
+	logger.debug('request url : ' +orgUrl);
 
-  if(!requestType){
-  	logger.debug('reqtype not set');
-  	res.render('index');
-  }else{
+	isByPassUrl(orgUrl)
+	.then(bypass=>{
 
-  	  requestType = requestType.toLowerCase(); 
+  		//check if needs to be loaded normally
+  		if(bypass){
+  			next();
+  		}else{
 
-	  let isjsonreq = false;
-	  let ismultipart = false;
-	  let isformurl = false;
+  			let requestType = req.get('Content-Type'); 
 
-	  if(requestType == 'application/json')
-	  	isjsonreq = true;
+  			//no content type is set then load react way
+  			if(!requestType){
+  				logger.debug('reqtype not set');
+  				res.render('index');
+  			}else{
 
-	  if(requestType.indexOf('multipart')>-1)
-	  	ismultipart = true;
+  				//else if an api call, then call the api
+  				requestType = requestType.toLowerCase(); 
 
-	  if(requestType.indexOf('form-urlencoded')>-1)
-	  	isformurl = true;
+  				let isjsonreq = false;
+  				let ismultipart = false;
+  				let isformurl = false;
 
-	  //if multipart or json request
-	  if (isjsonreq || ismultipart || isformurl) {
+  				if(requestType == 'application/json')
+  					isjsonreq = true;
 
-	  	logger.info('application/json or multipart request or form data');
-	  	next();
-	  }else{
+  				if(requestType.indexOf('multipart')>-1)
+  					ismultipart = true;
 
-	  	logger.info('not xhr method');
-	  	res.render('index');
-	  }
-  }
+  				if(requestType.indexOf('form-urlencoded')>-1)
+  					isformurl = true;
+
+		  //if multipart or json request
+		  if (isjsonreq || ismultipart || isformurl) {
+
+		  	logger.debug('application/json or multipart request or form data');
+		  	next();
+		  }else{
+
+		  	logger.debug('not xhr method');
+		  	res.render('index');
+		  }
+		}
+
+	}
+
+});
 }
