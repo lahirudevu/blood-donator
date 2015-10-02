59e8ce09051e2e1dcf386ff48ab64305d8ab8042
Gayan Witharana <vgayancharith@gmail.com>
2015-10-02 22:43:05 +0530

    add upload files into public/images/<current-date> folder

--- server/api/controllers/ResourceController.js
+++ server/api/controllers/ResourceController.js
@@ -1,33 +1,27 @@
 import express from 'express';
 import fs from 'fs';
 import multer  from 'multer';
+import policies from '../policies/';
+import UtilMethods from '../helpers/UtilMethods';
 
 var router = express.Router();
-var upload = multer({ dest: appRoot + '/public/images/'});
 
-router.post('/upload', (req, res) => {
-    console.log(req.files.image);
-    var tmp_path = req.files.image.path;
-    // set where the file should actually exists - in this case it is in the "images" directory
-    var target_path = appRoot + '/public/images/' + req.files.image.name;
-    // move the file from the temporary location to the intended location
-    fs.rename(tmp_path, target_path, function(err) {
-        if (err) throw err;
-        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-        fs.unlink(tmp_path, function() {
-            if (err) throw err;
-            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
-        });
-    });
+let storage = multer.diskStorage({
+	destination: function(req, file, cb) {
+		cb(null, appRoot + '/public/images/' + UtilMethods.calculateCurrentDate());
+	},
+	filename: function(req, file, cb) {
+		cb(null, file.originalname + '-' + Date.now());
+	}
 });
 
+let upload = multer({ storage: storage });
+
 // accept one file where the name of the form field is named photho
-// router.post('/upload', upload.single('image'), (req, res) => {
-//     logger.debug('=============== start ================');
-//     console.log(req.body) // form fields
-//     console.log(req.file) // form files
-//     logger.debug('=============== end ================');
-//     res.status(200).send('success');
-// });
+router.post('/upload', policies.uploadDirectory, upload.single('image'), (req, res) => {
+	logger.debug('uploading file...');
+    logger.info(req.file) // form files
+    res.status(200).send('success');
+});
 
 export default router;
