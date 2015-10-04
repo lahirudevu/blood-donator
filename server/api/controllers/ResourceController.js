55f0a7d3254fce098271a34479cf8bbe326ec512
Gayan Witharana <vgayancharith@gmail.com>
2015-10-04 10:24:46 +0530

    add es6 syntax to helpers/UtilMethods file

--- server/api/controllers/ResourceController.js
+++ server/api/controllers/ResourceController.js
@@ -4,27 +4,30 @@ import multer  from 'multer';
 import policies from '../policies/';
 import UtilMethods from '../helpers/UtilMethods';
 
-var router = express.Router();
-
+const router = express.Router();
+//initialize storage variable for uploading files
 let storage = multer.diskStorage({
-	destination: function(req, file, cb) {
-		cb(null, appRoot + '/public/images/' + UtilMethods.calculateCurrentDate());
-	},
-	filename: function(req, file, cb) {
-		const lastDotIndex = file.originalname.lastIndexOf('.');
-		const fileName = file.originalname.substr(0, lastDotIndex);
-		const fileExtenstion = file.originalname.substr(lastDotIndex + 1 );
-		cb(null, fileName + '-' + Date.now() + '.' + fileExtenstion);
-	}
-});
-
+		destination: function(req, file, cb) {
+			cb(null, appRoot + '/public/images/' + UtilMethods.calculateCurrentDate());
+		},
+		filename: function(req, file, cb) {
+			const lastDotIndex = file.originalname.lastIndexOf('.');
+			const fileName = file.originalname.substr(0, lastDotIndex);
+			const fileExtenstion = file.originalname.substr(lastDotIndex + 1);
+			cb(null, fileName + '-' + Date.now() + '.' + fileExtenstion);
+		}
+	});
 let upload = multer({ storage: storage });
 
 // upload file
 router.post('/upload', policies.uploadDirectory, upload.single('image'), (req, res) => {
 	logger.debug('uploading file...');
     logger.info(req.file) // form files
-    res.status(200).send('success');
+    if (req.file) {
+    	res.status(200).send('success');
+    } else {
+    	res.status(200).send('not found');
+    }    
 });
 
 export default router;
--- server/api/helpers/UtilMethods.js
+++ server/api/helpers/UtilMethods.js
@@ -1,35 +1,36 @@
 import crypto from 'crypto';
+import multer from 'multer';
 
-export default {
-
-	validateEmail : function(email) {
+class UtilMethods{
+	validateEmail(email) {
 	    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 	    return re.test(email);
-	},
+	}
 
-	validateDate : function(date) {
+	validateDate(date) {
 		//validate date
 	    return true;
-	},
+	}
 
-	validateTime : function(date) {
+	validateTime(date) {
 		//validate time
 	    return true;
-	},
+	}
 
-	generateRandomString: function(length){
+	generateRandomString(length){
 		return crypto.randomBytes(Math.ceil(length * 3 / 4))
         .toString('base64')   // convert to base64 format
         .slice(0, length)        // return required number of characters
         .replace(/\+/g, '0')  // replace '+' with '0'
         .replace(/\//g, '0'); // replace '/' with '0'
-	},
+	}
 
 	//get current date to the format of 'yyyy-mm-dd'
-	calculateCurrentDate: function() {
+	calculateCurrentDate() {
 		let date = new Date();
 		// let datevalues = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
 		return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
 	}
+}
 
-};
+export default new UtilMethods();
